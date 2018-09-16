0636fecd7a995de96fe21217eaf91364eb23ce93
Bush1do <Scorpius2011@yahoo.com>
2018-09-16 11:23:19 -0400

    9/16/18
    --Added Photography
    --Added Instagram
    --Removed Navigation
    --Removed Message box
    --Moved View Homepage button

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var max_particles = 5000;
+var max_particles = 1000;
 var particles = [];
 var frequency = 1;
 var init_num = max_particles;
